d4ef5caab525a3a9c3743c171abcaa5dff42f30e
Vilius <va96721@gmail.com>
2025-06-01 12:39:57 +0100

    Select multiple doesnt work. Need more research

--- FloEvent.Web/Controllers/MenusController.cs
+++ FloEvent.Web/Controllers/MenusController.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FloEvent.Catering.Data;
 using FloEvent.Web.ViewModels;
@@ -19,31 +22,53 @@ namespace FloEvent.Web.Controllers
         // GET: Menu
         public async Task<IActionResult> Index()
         {
-            var menus = await _context.Menus.ToListAsync();
-            var viewModels = menus.Select(m => new MenuViewModel { Menu = m }).ToList();
-            return View(viewModels);
+          var viewModel = await _context.Menus
+                .Select(m => new MenuViewModel
+                {
+                    MenuId = m.MenuId,
+                    MenuName = m.MenuName
+                })
+                .ToListAsync();
+
+            return View(viewModel);
         }
 
         // GET: Menu/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
             var menu = await _context.Menus
                 .Include(m => m.MenuFoodItems)
+                    .ThenInclude(mf => mf.FoodItem)
                 .FirstOrDefaultAsync(m => m.MenuId == id);
 
-            if (menu == null) return NotFound();
+            if (menu == null)
+            {
+                return NotFound();
+            }
 
-            return View(new MenuViewModel { Menu = menu });
+            return View(menu);
         }
+
 
         // GET: Menu/Create
         public IActionResult Create()
         {
-            return View(new MenuViewModel());
-        }
+            var viewModel = new MenuViewModel
+            {
+                FoodItemOptions = _context.FoodItems.Select(fi => new SelectListItem
+                {
+                    Value = fi.FoodItemId.ToString(),
+                    Text = fi.Name
+                }).ToList()
+            };
 
+            return View(viewModel);
+        }
         // POST: Menu/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -51,22 +76,58 @@ namespace FloEvent.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(viewModel.Menu);
+                var menu = new Menu
+                {
+                    MenuName = viewModel.MenuName,
+                    MenuFoodItems = viewModel.SelectedFoodItemIds
+                        .Select(id => new MenuFoodItems
+                        {
+                            FoodItemId = id
+                        }).ToList()
+                };
+
+                _context.Add(menu);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            // Repopulate options if ModelState is invalid
+            viewModel.FoodItemOptions = await _context.FoodItems
+                .Select(fi => new SelectListItem
+                {
+                    Value = fi.FoodItemId.ToString(),
+                    Text = fi.Name
+                }).ToListAsync();
+
             return View(viewModel);
         }
+
 
         // GET: Menu/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null) return NotFound();
 
-            var menu = await _context.Menus.FindAsync(id);
+            var menu = await _context.Menus
+                .Include(m => m.MenuFoodItems)
+                .FirstOrDefaultAsync(m => m.MenuId == id);
+
+
             if (menu == null) return NotFound();
 
-            return View(new MenuViewModel { Menu = menu });
+            var viewModel = new MenuViewModel
+            {
+                MenuId = menu.MenuId,
+                MenuName = menu.MenuName,
+                SelectedFoodItemIds = menu.MenuFoodItems.Select(mfi => mfi.FoodItemId).ToList(),
+                FoodItemOptions = _context.FoodItems.Select(fi => new SelectListItem
+                {
+                    Value = fi.FoodItemId.ToString(),
+                    Text = fi.Name
+                }).ToList()
+            };
+
+            return View(viewModel);
         }
 
         // POST: Menu/Edit/5
@@ -74,25 +135,40 @@ namespace FloEvent.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, MenuViewModel viewModel)
         {
-            if (id != viewModel.Menu.MenuId) return NotFound();
+            if (id != viewModel.MenuId) return NotFound();
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(viewModel.Menu);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MenuExists(viewModel.Menu.MenuId))
-                        return NotFound();
-                    else
-                        throw;
-                }
-                return RedirectToAction(nameof(Index));
+                viewModel.FoodItemOptions = await _context.FoodItems
+                    .Select(fi => new SelectListItem
+                    {
+                        Value = fi.FoodItemId.ToString(),
+                        Text = fi.Name
+                    }).ToListAsync();
+                return View(viewModel);
             }
-            return View(viewModel);
+
+            var menu = await _context.Menus
+                .Include(m => m.MenuFoodItems)
+                .FirstOrDefaultAsync(m => m.MenuId == id);
+
+            if (menu == null) return NotFound();
+
+            menu.MenuName = viewModel.MenuName;
+
+            // Clear and reassign food items
+            menu.MenuFoodItems.Clear();
+            foreach (var foodItemId in viewModel.SelectedFoodItemIds)
+            {
+                menu.MenuFoodItems.Add(new MenuFoodItems
+                {
+                    MenuId = id,
+                    FoodItemId = foodItemId
+                });
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Menu/Delete/5
@@ -105,7 +181,13 @@ namespace FloEvent.Web.Controllers
 
             if (menu == null) return NotFound();
 
-            return View(new MenuViewModel { Menu = menu });
+            var viewModel = new MenuViewModel
+            {
+                MenuId = menu.MenuId,
+                MenuName = menu.MenuName
+            };
+
+            return View(viewModel);
         }
 
         // POST: Menu/Delete/5
@@ -113,18 +195,23 @@ namespace FloEvent.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var menu = await _context.Menus.FindAsync(id);
-            if (menu != null)
-            {
-                _context.Menus.Remove(menu);
-                await _context.SaveChangesAsync();
-            }
-            return RedirectToAction(nameof(Index));
-        }
+            var menu = await _context.Menus
+                .Include(m => m.MenuFoodItems)
+                .FirstOrDefaultAsync(m => m.MenuId == id);
 
-        private bool MenuExists(int id)
-        {
-            return _context.Menus.Any(e => e.MenuId == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            // First remove related join table entries
+            _context.MenuFoodItems.RemoveRange(menu.MenuFoodItems);
+
+            // Then remove the Menu itself
+            _context.Menus.Remove(menu);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- FloEvent.Web/ViewModels/MenuViewModel.cs
+++ FloEvent.Web/ViewModels/MenuViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using FloEvent.Catering.Data;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace FloEvent.Web.ViewModels
 {
     public class MenuViewModel
     {
-        public Menu Menu { get; set; } = new Menu();
+
+        [Required]
+        public int MenuId { get; set; }
+
+        [Required]
+        public string MenuName { get; set; }
+
+        public List<int> SelectedFoodItemIds { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> FoodItemOptions { get; set; } = new List<SelectListItem>();
     }
 }
